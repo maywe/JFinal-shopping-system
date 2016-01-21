4de1895e322f11356803bc41c92f83d93123338b
chen229801 <229801394@qq.com>
2016-01-21 23:34:15 +0800

    我的web购物网站，helloword

--- src/com/base/ctrl/HelloWorld.java
+++ src/com/base/ctrl/HelloWorld.java
@@ -1,12 +1,18 @@
 package com.base.ctrl;
 
+import java.util.Date;
+
 /**
  * Created by ChenMW on 2016-01-21.
  */
 public class HelloWorld {
 
-    public static void main(){
+    public static void main(String [] args){
         ///
+        Date date = new Date();
+        System.out.println(date.getTime());
+
+
     }
 
 }
